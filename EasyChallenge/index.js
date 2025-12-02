83019ce41534617196b3e95fa3dc201c486087ba
Angel Martin <angelmartin@Angel-Administrators-MacBook-Pro.local>
2025-12-02 10:52:57 -0500

    update very hard challenge and easy challenge

--- EasyChallenge/index.js
+++ EasyChallenge/index.js
@@ -1,22 +1,38 @@
-
-const exercise = exerciseOfTheDay();
-const resultElement = document.getElementById("resultOfExercise")
-
 function exerciseOfTheDay(){
-    const message = "Today's exercise is :";
+    const message = "Today's exercise is ";
 
-    return function(NameofExercise){
-        const output = `${message} ${NameofExercise}`;
-    
+    return function (NameofExercise){
+        return `${message} : ${NameofExercise}`;
+    };
+}
+ const exercise = exerciseOfTheDay();
+
+ const resultElement =document.getElementById("resultOfExercise");
+
+ const button = document.getElementById("exerciseButton");
+
+ button.addEventListener("click", function (){
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    let chosenExercise;
+
+    if (randomNumber === 1){
+        chosenExercise = "Swimming";
+    } else if (randomNumber === 2){
+        chosenExercise = "Basketball";
+    } else if (randomNumber === 3){
+        chosenExercise = "Pickleball";
+    } else if (randomNumber === 4){
+        chosenExercise = "Archery";
+    } else if (randomNumber === 5){
+        chosenExercise = "Power Lifting";
+    } else {
+        chosenExercise = "Soccer";
+    }
+
+    const output = exercise(chosenExercise);
+
     console.log(output);
 
     resultElement.textContent = output;
-    };
-}
-
-console.log(exercise("Swimming"));
-console.log(exercise("Basketball"));
-console.log(exercise("Pickleball"));
-console.log(exercise("Archery"));
-console.log(exercise("Power Lifting"));
-console.log(exercise("Soccer"));
+ });
